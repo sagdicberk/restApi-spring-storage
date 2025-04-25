ea1c446fb628fa53a69f5e46703555672edabc3d
sagdicberk <sagdicberk@gmail.com>
2025-04-25 11:54:57 +0300

    undue imports are removed

--- src/main/java/com/repsy/restapi/model/PackageMeta.java
+++ src/main/java/com/repsy/restapi/model/PackageMeta.java
@@ -1,8 +1,6 @@
 package com.repsy.restapi.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import org.hibernate.annotations.Cascade;
 
 import java.util.ArrayList;
 import java.util.List;
